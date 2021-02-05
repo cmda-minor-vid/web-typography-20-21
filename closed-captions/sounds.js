dafdc6b4a964d59dd01e9369b738ca5d0a169c46
maxwieringa <maxwieringa01@gmail.com>
2021-02-05 15:35:13 +0100

    Kleur tekst van voice3 aangepast. Sounds toegevoegd om daarmee de achtergrond te veranderen.

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,16 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+  0,
+  9,
+  12,
+  15,
+  16.5,
+  20,
+  24,
+  25.8,
+  31,
+  32,
+  34,
+  86
 ];
